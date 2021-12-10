5fe187b7b71373c7859438e7fc6ddc8532fae034
Etienne Dilocker <etienne@dilocker.de>
2021-12-10 14:07:19 +0100

    fix config for text model, if it's a clip model

--- meta.py
+++ meta.py
@@ -6,7 +6,12 @@ class Meta:
 
   def __init__(self, clip_model_path, text_model_path):
     self.clip_config = CLIPConfig.from_pretrained(clip_model_path + '/0_CLIPModel')
-    self.text_config = AutoConfig.from_pretrained(text_model_path)
+    try:
+      # try as if it was a regular hf model
+      self.text_config = AutoConfig.from_pretrained(text_model_path)
+    except (RuntimeError, TypeError, NameError, Exception, EnvironmentError) as e:
+      # no try as if it's a ST CLIP model
+      self.text_config = AutoConfig.from_pretrained(text_model_path + '/0_CLIPModel')
 
   def get(self):
     return {
